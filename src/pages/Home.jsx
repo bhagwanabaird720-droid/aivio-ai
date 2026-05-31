126e17655e7489d85220b4e247b0254d1b2d9123
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-05-31 13:01:19 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,11 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Welcome To AIVIO 😎🚀
+      </h1>
+    </div>
+  )
+}
 
+export default Home
